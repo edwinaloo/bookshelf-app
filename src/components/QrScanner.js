5a0e25af947c75892d1a1aaa29a3c6bd8c13db37
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-12-01 17:50:44 +0300

    created Qr code scanner for the library system

---
+++ src/components/QrScanner.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader';
+
+const QrScanner = () => {
+  const [result, setResult] = useState('');
+
+  const handleScan = (data) => {
+    if (data) {
+      setResult(data);
+      // Redirect to the scanned book's individual page
+      // You can use react-router-dom for navigation
+    }
+  };
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+
+  return (
+    <div>
+      <h2>QR Code Scanner</h2>
+      <QrReader delay={300} onError={handleError} onScan={handleScan} />
+      {result && <p>Scanned Result: {result}</p>}
+    </div>
+  );
+};
+
+export default QrScanner;
